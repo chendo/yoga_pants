27a72238278746b504775e1c284631009e1ec6fc
Jack Chen (chendo) <yoga_pants@chen.do>
2012-06-05 17:21:51 +1000

    VCR all the things!

--- spec/integration/basic_spec.rb
+++ spec/integration/basic_spec.rb
@@ -1,3 +1,4 @@
+require "spec_helper"
 require "yoga_pants/client"
 
 module YogaPants
@@ -7,48 +8,58 @@ module YogaPants
     end
 
     before(:all) do
-      if subject.exists?("/yoga_pants_test")
-        subject.delete("/yoga_pants_test")
-      end
-      subject.post("/yoga_pants_test")
-      subject.put("/yoga_pants_test/doc/_mapping", :body => {
-        :doc => {
-          :properties => {
-             :foo => {:type => 'string'}
+      VCR.use_cassette('before_block') do
+        if subject.exists?("/yoga_pants_test")
+          subject.delete("/yoga_pants_test")
+        end
+        subject.post("/yoga_pants_test")
+        subject.put("/yoga_pants_test/doc/_mapping", :body => {
+          :doc => {
+            :properties => {
+               :foo => {:type => 'string'}
+            }
           }
-        }
-      })
+        })
+      end
     end
 
     it "indexes a valid document" do
-      subject.post("/yoga_pants_test/doc/1", :body => {
-        :foo => 'bar'
-      })
-      subject.get("/yoga_pants_test/doc/1").should == {
-        '_index' => 'yoga_pants_test',
-        '_type' => 'doc',
-        '_id' => '1',
-        '_version' => 1,
-        'exists' => true,
-        '_source' => {
-          'foo' => 'bar'
+      VCR.use_cassette('indexing') do
+        subject.post("/yoga_pants_test/doc/1", :body => {
+          :foo => 'bar'
+        })
+        subject.get("/yoga_pants_test/doc/1").should == {
+          '_index' => 'yoga_pants_test',
+          '_type' => 'doc',
+          '_id' => '1',
+          '_version' => 1,
+          'exists' => true,
+          '_source' => {
+            'foo' => 'bar'
+          }
         }
-      }
+      end
     end
 
     it "raises an exception on missing documents" do
-      expect { subject.get("/yoga_pants_test/doc/not_exist") }.should raise_error(Client::RequestError, "Error performing HTTP request: 404 Not Found")
+      VCR.use_cassette('missing') do
+        expect { subject.get("/yoga_pants_test/doc/not_exist") }.should raise_error(Client::RequestError, "Error performing HTTP request: 404 Not Found")
+      end
     end
 
     it "raises an exception on an invalid request" do
-      expect { subject.get("/this_does_not_exist") }.to raise_error(Client::RequestError, "Error performing HTTP request: 400 Bad Request")
+      VCR.use_cassette('invalid_request') do
+        expect { subject.get("/this_does_not_exist") }.to raise_error(Client::RequestError, "Error performing HTTP request: 400 Bad Request")
+      end
     end
 
     it "raises an exception when ES returns a hash with an error object" do
-      subject.post("/yoga_pants_test/doc/1", :body => {
-        :foo => 'bar'
-      })
-      expect { subject.get("/yoga_pants_test/doc/1/_mlt?min_term_freq=invalid") }.to raise_error(Client::RequestError, "Failed to parse int parameter [min_term_freq] with value [invalid]")
+      VCR.use_cassette('error') do
+        subject.post("/yoga_pants_test/doc/1", :body => {
+          :foo => 'bar'
+        })
+        expect { subject.get("/yoga_pants_test/doc/1/_mlt?min_term_freq=invalid") }.to raise_error(Client::RequestError, "ElasticSearch Error: Failed to parse int parameter [min_term_freq] with value [invalid]")
+      end
     end
 
   end
@@ -63,7 +74,9 @@ module YogaPants
       let(:host) { "http://localhost:1" } # Unlikely to be anything running on this port
 
       it "raises an RequestError" do
-        expect { subject.exists?("/foo") }.to raise_error(Client::RequestError, "Connection refused to http://localhost:1")
+        VCR.use_cassette('connection_refused') do
+          expect { subject.exists?("/foo") }.to raise_error(Client::RequestError, "Connection refused to http://localhost:1")
+        end
       end
     end
 
@@ -71,7 +84,9 @@ module YogaPants
       let(:host) { "http://192.168.34.58" } # Ewww but yeah.
 
       it "raises an RequestError" do
-        expect { subject.exists?("/foo") }.to raise_error(Client::RequestError, "Connection timed out to #{host}")
+        VCR.use_cassette('timed_out') do
+          expect { subject.exists?("/foo") }.to raise_error(Client::RequestError, "Connection timed out to #{host}")
+        end
       end
     end
 
