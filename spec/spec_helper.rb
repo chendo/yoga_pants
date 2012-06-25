5a26be251ec97fc9a6d02eaaeeec6e4648e1a43d
Jack Chen (chendo) <yoga_pants@chen.do>
2012-06-25 14:40:01 +1000

    Add some environment variable toggles for VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,6 @@ require 'yoga_pants'
 VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true if ENV['ALLOW_NO_CASSETTE']
+  c.ignore_localhost = true if ENV['INTEGRATION']
 end
